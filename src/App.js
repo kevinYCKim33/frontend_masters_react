5376caf7137a5d90fffa35b9e6b8c17ce75ae4bf
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-08-02 23:37:46 -0700

    jsx section finished

--- src/App.js
+++ src/App.js
@@ -2,33 +2,41 @@ import React from "react";
 // import ReactDOM from "react-dom"; // the more familiar syntax;
 import { render } from "react-dom"; // i want to import JUST render from ReactDOM
 // quasi-destructuring
-import Pet from "Pet";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }), //stamped the rubber stamp three times
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese"
+  //   }), //stamped the rubber stamp three times
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockatiel"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Mixed"
+  //   })
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 // an App is like a stamp...it's useless without it being on something...
 // createElement...is like stamping the thing onto the DOM
 // components in react: something that returns markup;
 render(
-  React.createElement(App), // actually invokes the const App
+  <App />, // actually invokes the const App
   document.getElementById("root") // then mounts it onto the root div
 );
 
--- src/Pet.js
+++ src/Pet.js
@@ -3,9 +3,18 @@ import React from "react";
 export default function Pet({ name, animal, breed }) {
   // es6 destructuring
   // create a variable name that will take on the value of the props object's name (key) value;
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed)
+  // ]);
+
+  // jsx just basically gets transpiled to React.createElement
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
