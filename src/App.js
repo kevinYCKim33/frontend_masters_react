6d4d1bf0a1e90e2e16849087a23084d9f6c0ed8a
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-08-02 20:11:22 -0700

    prettier setup

--- src/App.js
+++ src/App.js
@@ -5,33 +5,29 @@ const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ])
-}
+    React.createElement("h2", {}, breed)
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "something-important" },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }), //stamped the rubber stamp three times
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      })
-    ]
-  );
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }), //stamped the rubber stamp three times
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
 };
 // an App is like a stamp...it's useless without it being on something...
 // createElement...is like stamping the thing onto the DOM
