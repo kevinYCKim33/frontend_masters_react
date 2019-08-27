4489e931943b7eb88aacdc063ef5a7844585240c
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-08-26 22:00:31 -0700

    done with ssr with node stream

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import React from "react";
-import { renderToString } from "react-dom/server";
+// import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server"; //
+// old idea: 1 big payload
+// new idea: do piece by piece
+
 import { ServerLocation } from "@reach/router";
 import fs from "fs";
 import App from "../src/App";
@@ -15,16 +19,39 @@ const app = express();
 
 app.use("/dist", express.static("dist"));
 
+// old version
+// app.use((req, res) => {
+//   const reactMarkup = (
+//     <ServerLocation url={req.url}>
+//       <App />
+//     </ServerLocation>
+//   );
+//
+//   res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+//
+//   res.end();
+// });
+
+// new version with node stream
 app.use((req, res) => {
+  res.write(parts[0]);
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
 
-  res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+  const stream = renderToNodeStream(reactMarkup);
 
-  res.end();
+  stream.pipe(
+    res,
+    { end: false }
+  );
+
+  stream.on("end", () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 console.log("listening on " + PORT);
